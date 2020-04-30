151c647d19b7cd5fa9ad45f2d5b4e48198c1dcf6
Keiver Pacheco <krpachecop@gmail.com>
2020-04-30 00:05:51 -0500

    Arreglo de maqueta

--- components/modal.js
+++ components/modal.js
@@ -4,7 +4,7 @@ Vue.component('Modal',{
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title header" id="exampleModalLabel">Ingresar datos nueva Persona</h5>
+                        <h4 class="modal-title header" id="exampleModalLabel">Ingresar datos nueva Persona</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                           <span aria-hidden="true">&times;</span>
                         </button>
@@ -15,9 +15,7 @@ Vue.component('Modal',{
                                 <div class="col-md-12">
                                     <div class="well well-sm">
                                         <form class="form-horizontal" method="post" @submit.prevent="enviar">
-                                            <fieldset>
-                                                <legend class="text-center header">Datos</legend>
-                                    
+                                            <fieldset>                           
                                                 <div class="row">
                                                    <div class="row ">                                                    
                                                         <div class="form-group"> 
@@ -28,50 +26,45 @@ Vue.component('Modal',{
                                                                 <option value="2">E</option>
                                                             </select>                                            
                                                             </div>     
-                                                        </div>     
-                                                               
+                                                        </div>                                                                
                                                         <div class="col-md-offside-2">
                                                             <div class="form-group">  
                                                                 <input id="cedula" name="cd" type="text" placeholder="Cedula" ref="ci" :value="persona.ci" class="form-control" />
-                                                             </div>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                    
 
-                                                    <div class="form-row">                                                          
+                                                    <div class="form-group">                                                   
+                                                        <div class="form-row">                                                          
                                                             <div class="col-md">
                                                                 <input id="fname" name="name" type="text" ref="nombre" :value="persona.nombre" placeholder="Nombre" class="form-control" />
                                                             </div>                                                     
                                                             <div class="col-md">
                                                                 <input id="lname" name="apelldo" type="text" ref="apellido" :value="persona.apellido" placeholder="Apellido" class="form-control" />
                                                             </div>                                                        
-                                                    </div>
-
-
-                                                    <div class="col-md-8">  
-                                                        <div class="form-check form-check-inline">
-                                                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
-                                                            <label class="form-check-label" for="inlineRadio1">Masculino</label>
-                                                        </div>
-                                                        <div class="form-check form-check-inline">
-                                                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
-                                                            <label class="form-check-label" for="inlineRadio2">Femenino</label>
-                                                        </div>
-                                                     </div>                                     
+                                                        </div>                    
+                                                    </div>                    
             
-                                                    <div class="form-group">
-                                                        <span class="col-md-1 col-md-offset-2 text-center"><svg class="bi bi-envelope-fill bigicon" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M.05 3.555L8 8.414l7.95-4.859A2 2 0 0014 2H2A2 2 0 00.05 3.555zM16 4.697l-5.875 3.59L16 11.743V4.697zm-.168 8.108L9.157 8.879 8 9.586l-1.157-.707-6.675 3.926A2 2 0 002 14h12a2 2 0 001.832-1.195zM0 11.743l5.875-3.456L0 4.697v7.046z"/>
-                                                        </svg></span>                                     
+                                                    <div class="form-group">                                                                                           
                                                         <div class="col-mx-5 md-6">
-                                                            <input id="email" name="email" type="text" ref="correo" :value="persona.correo" placeholder="Correo" class="form-control" />
+                                                            <input id="email" name="email" type="text" ref="correo" :value="persona.correo" placeholder="Edad" class="form-control" />
                                                         </div>
-                                                    </div>           
-                                                </div>
-                                            </fieldset>
-                                                    
-                                            <button type="submit" class="btn btn-primary">Submit</button>
-                                                    
+                                                    </div>  
+                                                    <div class="form-group">
+                                                        <div class="col-md">  
+                                                            <div class="form-check form-check-inline">
+                                                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                                                                <label class="form-check-label" for="inlineRadio1">Masculino</label>
+                                                            </div>
+                                                            <div class="form-check form-check-inline">
+                                                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                                                                <label class="form-check-label" for="inlineRadio2">Femenino</label>
+                                                        </div>
+                                                    </div>                      
+                                                </div>                      
+                                            </div>
+                                            </fieldset>                                                    
+                                            <button type="submit" class="btn btn-success float-right">Submit</button>                                                    
                                         </form>
                                     </div>
                                 </div>
--- components/tablas.js
+++ components/tablas.js
@@ -18,25 +18,25 @@ Vue.component('Tabla', {
           <tr v-for="(persona, index) in personas">
                             <td>{{ persona.ci }}</td>
   
-      <td>            
+          <td>            
             <span v-if="act && idac == index">
                 <input v-model="nombreA" type="text" class="form-control">
             </span>
             <span v-else>
                               {{ persona.nombre }}
             </span>
-        </td>
+          </td>
   
-      <td>            
+           <td>            
             <span v-if="act && idac == index">
                 <input v-model="apellidoA" type="text" class="form-control">
             </span>
             <span v-else>
                               {{ persona.apellido }}
             </span>
-        </td>
+          </td>
   
-                      
+             <td>         
             <span v-if="act && idac == index">
                <input v-model="sexoA" type="text" class="form-control">
             </span>
@@ -44,6 +44,7 @@ Vue.component('Tabla', {
                                {{ persona.sexo }}
             
             </span>
+            </td>
                         
                 <td>            
             <span v-if="act && idac == index">
@@ -53,16 +54,16 @@ Vue.component('Tabla', {
                               {{ persona.correo }}
             </span>
         </td>
-        <td>        
+              
                     
 
                     <td>  
                         <span v-if="act && idac == index">
-                            <button @click="submitActualizar(index)" class="btn btn-success">Guardar</button>
+                            <button @click="submitActualizar(index)" class="btn btn-info">Guardar</button>
                         </span>
                         
                         <span v-else>
-                            <button data-target="exampleModal" @click="verActualizar(index)" class="btn     btn-warning">Actualizar</button>
+                            <button data-target="exampleModal" @click="verActualizar(index)" class="btn btn-info">Actualizar</button>
                             <button @click="borrar" class="btn btn-danger">Borrar</button>
                         </span>
                     </td>
